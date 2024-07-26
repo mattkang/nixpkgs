5460bcdd578581ca782afd1d348db63e77bb27e3
MattKang <2027430+MattKang@users.noreply.github.com>
2024-07-25 21:28:08 -0400

    Add MattKang to maintainer list

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -13511,6 +13511,12 @@
     githubId = 279868;
     name = "Matti Kariluoma";
   };
+  MattKang = {
+    email = "wavy-wisdom-volley@duck.com";
+    github = "MattKang";
+    githubId = 2027430;
+    name = "Matthew Kang";
+  };
   mattpolzin = {
     email = "matt.polzin@gmail.com";
     github = "mattpolzin";
